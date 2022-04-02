8f5a7b1d9fede96b5b17728981bc89187f46478e
Bret Barkelew <brbarkel@microsoft.com>
2022-04-02 00:22:06 -0700

    Clean up the ConOut implementation

--- core_con_out/src/lib.rs
+++ core_con_out/src/lib.rs
@@ -4,7 +4,6 @@
 
 #![no_std]
 
-use core::cell::RefCell;
 use core::fmt::Write;
 use core::ptr::NonNull;
 use r_efi::efi;
@@ -12,51 +11,51 @@ use spin::Mutex;
 
 type UefiResult<T> = Result<T, efi::Status>;
 
-pub struct ConOut {
-    inner: NonNull<efi::protocols::simple_text_output::Protocol>,
-}
-
 const CON_OUT_BUFFER_SIZE: usize = 0x400;
+pub struct ConOut {
+    inner: Option<NonNull<efi::protocols::simple_text_output::Protocol>>,
+    buffer: [u16; CON_OUT_BUFFER_SIZE],
+}
+// NOTE: UEFI isn't thread safe anyway, so we're not introducing too many problems
+//       by allowing the protocol to be shared.
+unsafe impl Sync for ConOut {}
+unsafe impl Send for ConOut {}
 
-static mut CON_OUT: Option<RefCell<ConOut>> = None;
-static mut CON_OUT_BUFFER: Mutex<[u16; CON_OUT_BUFFER_SIZE]> =
-    Mutex::new([0u16; CON_OUT_BUFFER_SIZE]);
+// TODO: Move the buffer into the struct and move the Mutex around the option.
+//       Before doing this, make sure you can still get to the pointer in a panic.
+//       Looks like mutex.force_unlock would get it done.
+static CON_OUT: Mutex<ConOut> = Mutex::new(ConOut {
+    inner: None,
+    buffer: [0u16; CON_OUT_BUFFER_SIZE],
+});
+
 
 impl ConOut {
-    fn new(st_ptr: *mut efi::SystemTable) -> UefiResult<Self> {
-        let st = unsafe { st_ptr.as_ref() }.ok_or(efi::Status::INVALID_PARAMETER)?;
-        NonNull::new(st.con_out)
-            .map(|inner| Self { inner })
-            .ok_or(efi::Status::INVALID_PARAMETER)
-    }
-
     pub unsafe fn init(st_ptr: *mut efi::SystemTable) -> UefiResult<()> {
-        let co = Self::new(st_ptr)?;
-        CON_OUT = Some(RefCell::new(co));
+        let st = st_ptr.as_ref().ok_or(efi::Status::INVALID_PARAMETER)?;
+        CON_OUT.lock().inner = NonNull::new(st.con_out);
         Ok(())
     }
 
     pub fn print(&mut self, out_string: &str) {
-        let co = self.inner.as_ptr();
-        let mut buffer = unsafe { CON_OUT_BUFFER.lock() };
-
-        let mut i: usize = 0;
-        for utf8byte in out_string.bytes() {
-            buffer[i] = match utf8byte {
-                // printable ASCII byte or newline
-                0x20..=0x7e | b'\n' | b'\r' => utf8byte as u16,
-                // not part of printable ASCII range
-                _ => 0xfe as u16,
-            };
-            i += 1;
-            if i > (CON_OUT_BUFFER_SIZE - 2) {
-                break;
+        if let Some(ref mut co) = self.inner {
+            let co = unsafe { co.as_mut() };
+            let mut i: usize = 0;
+            for utf8byte in out_string.bytes() {
+                self.buffer[i] = match utf8byte {
+                    // printable ASCII byte or newline
+                    0x20..=0x7e | b'\n' | b'\r' => utf8byte as u16,
+                    // not part of printable ASCII range
+                    _ => 0xfe as u16,
+                };
+                i += 1;
+                if i > (self.buffer.len() - 2) {
+                    break;
+                }
             }
-        }
-        buffer[i] = 0u16;
+            self.buffer[i] = 0u16;
 
-        unsafe {
-            ((*co).output_string)(co, buffer.as_mut_ptr() as *mut efi::Char16);
+            (co.output_string)(co as *mut _, self.buffer.as_mut_ptr() as *mut _);
         }
     }
 }
@@ -68,23 +67,17 @@ impl core::fmt::Write for ConOut {
     }
 }
 
-// This function is especially unsafe in that it will seize the RefCell regardless
-// of whether it is currently borrowed. Only to be used in panic situations
+// This function is especially unsafe in that it will seize the Mutex regardless
+// of whether it is currently locked. Only to be used in panic situations
 // when there is no expectation to return.
 pub unsafe fn print_panic(args: ::core::fmt::Arguments) {
-    CON_OUT.as_mut().map(|rc| rc.get_mut().write_fmt(args));
+    CON_OUT.force_unlock();
+    _ = CON_OUT.lock().write_fmt(args);
 }
 
 #[doc(hidden)]
 pub fn _print(args: ::core::fmt::Arguments) {
-    match unsafe { CON_OUT.as_mut() } {
-        Some(co) => {
-            co.borrow_mut()
-                .write_fmt(args)
-                .expect("error in ConOut write");
-        }
-        _ => (),
-    }
+    CON_OUT.lock().write_fmt(args).expect("error in ConOut write");
 }
 
 #[macro_export]
